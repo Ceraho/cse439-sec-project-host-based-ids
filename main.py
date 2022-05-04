c3a66174fff798f110ce594d60affd3409464295
ecerrahoglu <erimcrhkn00@gmail.com>
2022-05-04 18:04:30 +0300

    new functionalities added and changes on pathnames

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import os
 import hashlib
+import string
+from collections import Counter
+
+
+def find_identical_files(some_list):
+    hash_list = list()
+    for i in range(len(some_list)):
+        hash_list.append(some_list[i][0])
+
+    d = Counter(hash_list)
+
+    identical_files_list = list([item for item in d if d[item] > 1])
+
+    final_list = list()
+    for i in range(len(identical_files_list)):
+        for j in range(len(some_list)):
+            if identical_files_list[i] == some_list[j][0]:
+                final_list.append(some_list[j])
+
+    print("\nIDENTICAL FILES ARE:\n")
+    for i in range(len(final_list)):
+        print(final_list[i])
 
 
 def hash_file(filename):
@@ -17,11 +39,38 @@ def hash_file(filename):
     return h.hexdigest()
 
 
-rootdir = 'IDS_DIRECTORY'
-list_of_hash_values = list()
+def hash_all_files(root_path):
+    x = list()
+    for subdir, dirs, files in os.walk(root_path):
+        for file in files:
+            x.append([hash_file(os.path.join(subdir, file)), os.path.join(subdir, file)])
 
-for subdir, dirs, files in os.walk(rootdir):
-    for file in files:
-        list_of_hash_values.append(hash_file(os.path.join(subdir, file)))
+    print("\nHASH VALUES ARE:\n")
+    for i in range(len(x)):      # We print every hashed value and their path to check.
+        print(x[i])
 
-print(list_of_hash_values)
+    return x
+
+
+def take_snapshot(root_path):
+    print("\nSNAPSHOT OPERATION STARTED...")
+    list_to_text = list(hash_all_files(root_path))
+
+    with open("Snapshots/snp_1.txt", 'w') as f:
+        f.write('\n'.join(str(e) for e in list_to_text))
+
+    print("\nSNAPSHOT SAVED.\n")
+
+
+# This is where we execute and demonstrate our IDS' functionalities.
+def main():
+    rootdir = 'DEMO_DIRECTORY'  # Define the root path's name where IDS will work on.
+    list_of_current_hash_values = hash_all_files(rootdir)  # Get hash values of every existing file in the given path/directory.
+
+    find_identical_files(list_of_current_hash_values)   # Find every identical file from their hash values and return them
+
+    take_snapshot(rootdir)      # Takes snapshot of the DEMO_DIRECTORY's current version. (Saves hash values and file path's as .txt file)
+
+
+if __name__ == "__main__":
+    main()
--- script_a.py
+++ script_a.py
@@ -2,22 +2,14 @@ import os
 import string
 import random
 
-rootdir = 'IDS_DIRECTORY'
+rootdir = 'DEMO_DIRECTORY'
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         # print(os.path.join(subdir, file))
         with open(os.path.join(subdir, file), 'w') as f:
             # f.write("This is a dummy text file to test Host Based IDS Project.")
-            if os.path.join(subdir, file) == 'IDS_DIRECTORY/SUB_DIRECTORY_1/sub_1_text_2.txt' or os.path.join(subdir, file) == 'IDS_DIRECTORY/text_4.txt':
+            if os.path.join(subdir, file) == 'DEMO_DIRECTORY/SUB_DIRECTORY_1/sub_1_text_2.txt' or os.path.join(subdir, file) == 'DEMO_DIRECTORY/text_4.txt':
                 f.write("This is a dummy text file to test Host Based IDS Project.")
             else:
                 f.write(''.join(random.choice(string.ascii_letters + string.digits) for _ in range(128)))
-
-# echo "# CSE439_Sec_Project-Host_Based_IDS" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/Ceraho/CSE439_Sec_Project-Host_Based_IDS.git
-# git push -u origin main

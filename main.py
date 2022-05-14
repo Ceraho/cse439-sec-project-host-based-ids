1d97f7f15f053b8e26edc5e50a45679944aeac71
ecerrahoglu <cerrahoglu.erim@gmail.com>
2022-05-14 17:12:13 +0300

    added AES encryption to encrypt snapshot files

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import os
 import hashlib
 import pickle
+import AES
+import filecmp
 from collections import Counter
 
 
-def edit_sub_text():
-    print("press 1 to edit sub_1_text_2.txt, press 2 to revert it to original text.")
+password = "SECURITY IS IMPORTANT 439 +-*/"
+
+
+def edit_some_file(filename):
+    print(f"press 1 to edit {filename} file (edited file will be modified in an unintended way,"
+          " causing conflicts to test IDS functionalities)")
+    if "sub_1_text_2.txt" in filename:
+        print("press 2 to revert sub_1_text_2.txt back to original")
+    print("press 0 to go back.")
+
     action = int(input("action: "))
+
     if action == 1:
         new_text = input("New text: ")
-        with open("DEMO_DIRECTORY/SUB_DIRECTORY_1/sub_1_text_2.txt", 'w') as f:
+        with open(filename, 'w') as f:
             f.write(new_text)
-    elif action == 2:
+    elif action == 2 and "sub_1_text_2.txt" in filename:
         with open("DEMO_DIRECTORY/SUB_DIRECTORY_1/sub_1_text_2.txt", 'w') as f:
             f.write("This is a dummy text file to test Host Based IDS Project.")
+    else:
+        pass
 
 
 def hash_file(filename):
     # make a hash object
-    h = hashlib.sha1()
+    h = hashlib.sha256()
     # open file for reading in binary mode
     with open(filename, 'rb') as file:
         # loop till the end of the file
@@ -69,24 +82,38 @@ def take_snapshot(root_path):
     print("\nSNAPSHOT OPERATION STARTED...")
     list_to_text = list(hash_all_files(root_path))
 
-    open_file = open("Snapshots/snapshot.txt", 'wb')
+    open_file = open("Snapshots/snapshot.txt", 'wb+')
     pickle.dump(list_to_text, open_file)
     open_file.close()
 
-    print("\nSNAPSHOT SAVED.\n")
+    # password = input("Password: ")
+    AES.encrypt(AES.getKey(password), "Snapshots/snapshot.txt")
+
+    print("\nSNAPSHOT SAVED AND ENCRYPTED.\n")
 
 
 def compare_to_latest_snapshot(root_path):
     print("\nCOMPARISON OPERATION HAS BEEN STARTED...\n")
 
+    # password = input("Password: ")
+    try:
+        AES.decrypt(AES.getKey(password), "Snapshots/(enc)snapshot.txt")
+    except:
+        print("Can't find encrypted snapshot file!")
+
+    check_if_modified = filecmp.cmp("Snapshots/snapshot.txt", "Snapshots/(dec)snapshot.txt", shallow=False)
+
+    if not check_if_modified:
+        print("WARNING: THE SNAPSHOT FILES HAS BEEN MODIFIED BY AN OUTSOURCE!")
+        return
+
     try:
         open_file = open("Snapshots/snapshot.txt", 'rb')
+        last_checkpoint_list = pickle.load(open_file)
+        open_file.close()
     except:
         print("No snapshot file exists yet!")
-        return 0
-
-    last_checkpoint_list = pickle.load(open_file)
-    open_file.close()
+        return
 
     current_hash_list = hash_all_files(root_path)
 
@@ -94,9 +121,9 @@ def compare_to_latest_snapshot(root_path):
     current_hash_list.sort()
 
     if last_checkpoint_list == current_hash_list:
-        print("\nIT ALL SEEMS FINE, NO MODIFICATIONS SINCE THE CHECKPOINT.")
+        print("\nIt all seems fine, no modifications since the checkpoint.")
     else:
-        print("\nMODIFIED FILES DETECTED:")
+        print("\nWARNING: MODIFIED FILES DETECTED IN THE DIRECTORY!")
 
 
 # This is where we execute and demonstrate our IDS' functionalities.
@@ -117,19 +144,35 @@ def main():
         print("1. Check for identical files in current state of the directory.")
         print("2. Take a snapshot of the current state of this directory.")
         print("3. Compare the current state with the last snapshot.")
-        print("4. (FOR DEMO PURPOSES) Edit sub_1_text_2.txt to test compare function.")
+        print("4. (FOR DEMO PURPOSES) Edit sub_1_text_2.txt to test compare to last checkpoint function.")
+        print("5. (FOR DEMO PURPOSES) Edit snapshot.txt to test if modifying original snapshot file case is covered.")
         print("0. Exit")
         action = int(input("action: "))
         if action == 1:
+            # Find every identical file from their hash values and return them.
             find_identical_files(rootdir)
         elif action == 2:
+            # Takes snapshot of the DEMO_DIRECTORY's current version.
+            # ... (Saves hash values and file path's as .txt file)
             take_snapshot(rootdir)
         elif action == 3:
+            # Start comparison with the latest snapshot and check if any file got modified.
             compare_to_latest_snapshot(rootdir)
         elif action == 4:
-            edit_sub_text()
+            # We use edit_some_file() func. to modify sub_1_text_2.txt after we run take_snapshot()
+            # ... and right before compare_to_latest_snapshot() in order to test and see if comparing
+            # ... to the latest snapshot works.
+            edit_some_file("DEMO_DIRECTORY/SUB_DIRECTORY_1/sub_1_text_2.txt")
+        elif action == 5:
+            # We use edit_some_file() func. to modify snapshot.txt after we run take_snapshot()
+            # ... and right before compare_to_latest_snapshot() in order to test and see if we can
+            # ... detect modified snapshot file.
+            edit_some_file("Snapshots/snapshot.txt")
         elif action == 0:
+            # Quit the program safely.
             break
+        else:
+            print("Invalid action, please try again.")
 
 
 if __name__ == "__main__":
